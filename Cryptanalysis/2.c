e2600e1f3d6c517df28f322ce97c3511321195ab
7-RED <a27032522@gmail.com>
2021-03-19 01:53:27 +0800

    CommentOutUpdate

--- Cryptanalysis/2.c
+++ Cryptanalysis/2.c
@@ -4,31 +4,31 @@
 
 int main(void)
 {
-	int n;
-	char input[1000]={0};
-	int count[26]={0};
-	int total=0;
+	int n;//輸入n列字串
+	char input[1000]={0};//輸入的文字
+	int count[26]={0};//字母26個
+	int total=0;//計數器
 
 
 	scanf("%d\n",&n);
 	while(n--)
 	{
-		fgets(input,1000,stdin);
+		fgets(input,1000,stdin);//fgets函數讀取字串
 		for(int y=0;y<strlen(input);y++)
 		{
-			if(input[y]>='a' && input[y]<='z')
+			if(input[y]>='a' && input[y]<='z')//字母小寫
 			{
-				count[input[y]-'a']++;
+				count[input[y]-'a']++;//把字母減掉小寫a，正規化
 				total++;
 			}
-			else if(input[y]>='A' && input[y]<='Z')
+			else if(input[y]>='A' && input[y]<='Z')//字母大寫
 			{
-				 count[input[y]-'A']++;
+				 count[input[y]-'A']++;//字母正規化
 				 total++;
 			}
 		}
 	}
-	for(int z=total;z>0;z--)
+	for(int z=total;z>0;z--)//計數迴圈
 		for(int a=0;a<26;a++)
 			if(count[a]==z)
 				printf("%c %d\n",a+'A',z);
--- DoomsDay/3.c
+++ DoomsDay/3.c
@@ -3,20 +3,20 @@
 
 int main(void)
 {
-	int line,m,d;
-	const int monthday[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-	const char *dayweek[]={ "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-	scanf("%d",&line);
+	int line,m,d;//line表幾行,m月份,d日期
+	const int monthday[]={0,31,28,31,30,31,30,31,31,30,31,30,31};//常數表每個月份的天數
+	const char *dayweek[]={ "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};//每週日期 從星期六開始（因為２０１１年1月1號是星期六從題意推算
+	scanf("%d",&line);//看資料有幾行
 	while(line--)
 	{
 		scanf("%d %d",&m,&d);
-		int totalday=0;
-		for(int i=0;i<m;i++)
+		int totalday=0;//歸零
+		for(int i=0;i<m;i++)//幾個月份
 		{
-			totalday+=monthday[i];
+			totalday+=monthday[i];//加天數
 		}
-		totalday+=d;
-		printf("%s\n",dayweek[totalday%7-1]);
+		totalday+=d;//加上最後的日期
+		printf("%s\n",dayweek[totalday%7-1]);//取餘數-1因為陣列從零開始
 	}
 	return 0;
 }
